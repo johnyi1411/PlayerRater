58fad338f97a79da600f7f3199c0f05a4b38ef7c
John Yi <john.yi1411@gmail.com>
2019-09-28 19:40:30 -0700

    Create database connection to postgres

---
+++ database/config/pg.config.example.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host: 'my.database-server.com',
+  port: 5432,
+  user: 'database-user',
+  database: 'mydatabase',
+  password: 'secretpassword!!',
+};
--- database/postgres.js
+++ database/postgres.js
@@ -1,1 +1,16 @@
-const pg = require('pg');
+const { Client } = require('pg');
+const config = require('./config/pg.config');
+
+const client = new Client(config);
+client.connect(err => {
+  if (err) {
+    console.error('connection error', err.stack)
+  } else {
+    console.log('pg connected')
+  }
+});
+
+client
+  .query('SELECT * from dishes')
+  .then(res => console.log(res.rows[0]))
+  .catch(e => console.error(e.stack));
